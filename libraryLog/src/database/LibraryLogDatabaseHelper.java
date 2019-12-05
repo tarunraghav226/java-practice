d9b03b1dc172f98d8b782c7aa24ba3a8a0c05972
tarunraghav226 <tarun.raghav226@gmail.com>
2019-12-05 20:14:44 +0530

    added database and user terminal interface added addToRelation method to issue book added returnBook method to return book

--- libraryLog/src/database/LibraryLogDatabaseHelper.java
+++ libraryLog/src/database/LibraryLogDatabaseHelper.java
@@ -1,17 +1,44 @@
 package database;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.Date;
 
 public class LibraryLogDatabaseHelper {
-    public void addToRelation(Connection connection,int id,int studentId,int bookID,String issueDate,String returnDate){
+    public int addToRelation(Connection connection,int studentId,int bookID){
         try{
             Statement statement=connection.createStatement();
-            Date IssueDate=new Date(issueDate);
-            System.out.println(IssueDate);
-            Date ReturnDate = new Date(returnDate);
-            String sql="insert into libraryLog values("+id+","+studentId+","+bookID+","+IssueDate+","+ReturnDate+");";
-            statement.executeUpdate(sql);
+            ResultSet resultSet = statement.executeQuery("select count(*) from libraryLog;");
+
+            int count=0;
+            while(resultSet.next()) {
+                count = resultSet.getInt(1);
+            }
+
+            String IssueDate=java.time.LocalDate.now().toString();
+            StringBuffer sql=new StringBuffer("insert into libraryLog (id,Student_ID,Book_No,Issue_Date) values(");
+            sql.append(Integer.valueOf(++count).toString()+",");
+            sql.append(Integer.valueOf(studentId).toString()+",");
+            sql.append(Integer.valueOf(bookID).toString()+",");
+            sql.append("'"+IssueDate+"');");
+            statement.executeUpdate(sql.toString());
+            return count;
+        }
+        catch (SQLException e){
+            System.out.println("SQLException Occurred");
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public void returnBook(Connection connection,int issueNumber){
+        try{
+            Statement statement=connection.createStatement();
+            String returnDate=java.time.LocalDate.now().toString();
+            StringBuffer sql=new StringBuffer("update libraryLog set Return_Date=");
+            sql.append("'"+returnDate+"' where ID=");
+            sql.append(Integer.valueOf(issueNumber).toString()+";");
+            statement.executeUpdate(sql.toString());
         }
         catch (SQLException e){
             System.out.println("SQLException Occurred");
--- libraryLog/src/database/LibraryLogMain.java
+++ libraryLog/src/database/LibraryLogMain.java
@@ -1,12 +1,23 @@
 package database;
 
+import java.sql.Connection;
+import java.util.Scanner;
+
 public class LibraryLogMain {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         LibraryLogDatabaseConnection connection=new LibraryLogDatabaseConnection();
         if(connection.makeConnection()){
             LibraryLogDatabaseHelper databaseHelper=new LibraryLogDatabaseHelper();
-            databaseHelper.addToRelation(connection.getConnection(),1,1,1,"5/12/2019","6/12/2019");
-
+            System.out.println("Do You Want To Issue Book");
+            if(scan.next().charAt(0)=='y') {
+                int issueNumber=databaseHelper.addToRelation(connection.getConnection(), 12, 1);
+                System.out.println("Your Issue Number Is "+issueNumber+"\nRemember This Number When You Come To Return Book");
+            }
+            else{
+                int issueNumber=scan.nextInt();
+                databaseHelper.returnBook(connection.getConnection(),issueNumber);
+            }
         }
     }
 }
